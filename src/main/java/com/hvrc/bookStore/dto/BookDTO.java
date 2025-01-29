ae885e912e84933bede94438fa16ef06e634e527
1231harsh <1206harsh@gmail.com>
2025-01-29 14:30:23 +0530

    reverted

--- src/main/java/com/hvrc/bookStore/dto/BookDTO.java
+++ src/main/java/com/hvrc/bookStore/dto/BookDTO.java
@@ -9,28 +9,15 @@ public class BookDTO {
     private String name;
     private String author;
     private String description;
-<<<<<<< HEAD
     private Double price;
     private Long quantity;
 
     public BookDTO(Long id,String name,String author ,String description ,Double price ,Long quantity) {
-=======
-    private String category;
-    private Double price;
-    private Long quantity;
-
-
-    public BookDTO(Long id,String name,String author ,String description ,String category, Double price ,Long quantity) {
->>>>>>> f00bc12 (Added new files to the project)
         this.id = id;
         this.name = name;
         this.author = author;
         this.description = description;
         this.price = price;
         this.quantity = quantity;
-<<<<<<< HEAD
-=======
-        this.category = category;
->>>>>>> f00bc12 (Added new files to the project)
     }
 }
