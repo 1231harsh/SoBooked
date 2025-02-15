ae12879476562e296f3ef2ed01ef6ea521e12e82
1231harsh <1206harsh@gmail.com>
2025-02-15 20:31:22 +0530

    Book deletion bug solved

--- src/main/java/com/hvrc/bookStore/dto/OrderMapper.java
+++ src/main/java/com/hvrc/bookStore/dto/OrderMapper.java
@@ -7,7 +7,7 @@ public class OrderMapper {
     public static OrderDTO toOrderDTO(Order order) {
         OrderDTO orderDTO = new OrderDTO();
         orderDTO.setId(order.getId());
-        orderDTO.setCustomerId(order.getCustomerId());
+        orderDTO.setCustomerId(order.getUser().getId());
         orderDTO.setPaymentId(order.getPaymentId());
         orderDTO.setTotalAmount(order.getTotalAmount());
         orderDTO.setOrderDate(order.getOrderDate());
